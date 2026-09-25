08d6436b60e6bf80885091e7ff9b182b86373612
Jens Wedin <jens@studiomanfred.com>
2026-09-25 19:23:59 +0200

    remote-for-sonos 0.2.5

--- Casks/remote-for-sonos.rb
+++ Casks/remote-for-sonos.rb
@@ -1,13 +1,14 @@
 # Homebrew cask template. scripts/release.sh fills in the version and sha256 and
 # pushes the result to the jens-wedin/homebrew-tap repository as Casks/remote-for-sonos.rb.
 cask "remote-for-sonos" do
-  version "0.2.4"
-  sha256 "60d1cee43f46aeb8e49c8a82c744caf112920044c482768f4c61065701671f93"
+  version "0.2.5"
+  sha256 "f29c2794ebe10d37d9625184c279859f202747585b78c7eebdc8c874a07cd38f"
 
   url "https://github.com/jens-wedin/sonos-remote/releases/download/v#{version}/Remote-for-Sonos-#{version}.zip"
   name "Remote for Sonos"
   desc "Menu bar controller for Sonos speakers on the local network"
   homepage "https://github.com/jens-wedin/sonos-remote"
+  auto_updates true
 
   depends_on macos: :tahoe
 
