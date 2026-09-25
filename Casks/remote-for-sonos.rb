86bfdd8f976003938de76984d2aff25a0564f686
Jens Wedin <jens@studiomanfred.com>
2026-09-25 09:55:45 +0200

    remote-for-sonos 0.2.4

--- Casks/remote-for-sonos.rb
+++ Casks/remote-for-sonos.rb
@@ -1,8 +1,8 @@
 # Homebrew cask template. scripts/release.sh fills in the version and sha256 and
 # pushes the result to the jens-wedin/homebrew-tap repository as Casks/remote-for-sonos.rb.
 cask "remote-for-sonos" do
-  version "0.2.3"
-  sha256 "6c60609665ca3ad63138426aed63f0168c70cd35ba56c804da75ec798bc4a89f"
+  version "0.2.4"
+  sha256 "60d1cee43f46aeb8e49c8a82c744caf112920044c482768f4c61065701671f93"
 
   url "https://github.com/jens-wedin/sonos-remote/releases/download/v#{version}/Remote-for-Sonos-#{version}.zip"
   name "Remote for Sonos"
